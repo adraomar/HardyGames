152d7187bd31117284674000f8d39a0613c731fb
adraomar <adra.omar2@gmail.com>
2022-07-31 23:36:14 -0300

    Agregado negritas al greeting

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ItemListContainer = (props) => {
     return(
-        <h1 className="h1 text-light"> Bienvenidos al ECommerce {props.titulo} tienda Online de Videojuegos, accesorios y merchandising.</h1>
+        <h1 className="h1 text-light"> Bienvenidos al ECommerce <strong>{props.titulo}</strong> tienda Online de Videojuegos, accesorios y merchandising.</h1>
     );
 }
 
