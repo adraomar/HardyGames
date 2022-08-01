a71ee1404a84a2686305a7c3f1914178b029ea79
adraomar <adra.omar2@gmail.com>
2022-07-31 22:14:15 -0300

    Diseño y contrucción de estilo de NavBar y vinculado a App.js

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import './navbar.css';
+import logo from './hardygames-logo.png';
 
 const Navbar = () => {
     return (
-        <h1>test</h1>
+        <header className="p-3 bg-dark text-white">
+            <div className="container-fluid">
+                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/#" className="d-flex align-items-start mb-2 mb-lg-0 text-white text-decoration-none">
+                        <img src={logo} alt="" />
+                    </a>
+
+                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li className="active"><a href="/#" className="nav-link px-2 text-white">Inicio</a></li>
+                        <li><a href="/#" className="nav-link px-2 text-white">Ofertas</a></li>
+                        <li><a href="/#" className="nav-link px-2 text-white">Sobre Nosotros</a></li>
+                    </ul>
+
+                    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                        <div className="input-group">
+                            <input type="text" className="form-control" placeholder="Buscar producto..." aria-label="Search" aria-describedby="btnBuscar"/>
+                            <button className="btn btn-outline-secondary" type="button" id="btnBuscar"><i className="bi bi-search"></i></button>
+                        </div>
+                    </form>
+
+                    <div className="text-end">
+                        <button type="button" className="btn btn-outline-light mx-3"><i className="bi bi-cart" style={{fontSize: 18}}></i><span className="badge text-bg-success ms-3">4</span></button>
+                        <button type="button" className="btn btn-outline-light me-2">Conectarse</button>
+                        <button type="button" className="btn btn-warning">Registrarse</button>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
