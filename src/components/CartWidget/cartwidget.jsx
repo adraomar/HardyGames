b53df95688c49c782a24200f24e10b2856904abf
adraomar <adra.omar2@gmail.com>
2022-08-21 06:55:32 -0300

    Creado componentes ItemList e Item y codigo base

--- src/components/CartWidget/cartwidget.jsx
+++ src/components/CartWidget/cartwidget.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ItemCount from '../ItemCount/ItemCount';
+
 
 const CartWidget = () => {
     return (
-        <button type="button" className="btn btn-outline-light mx-3"><i className="bi bi-cart" style={{ fontSize: 18 }}></i><span className="badge text-bg-success ms-3">4</span></button>
+        <button type="button" className="btn btn-outline-light mx-3"><i className="bi bi-cart" style={{ fontSize: 18 }}></i><span className="badge text-bg-success ms-3">0</span></button>
     );
 }
 
