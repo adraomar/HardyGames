e52819e99c0ed29aa121ab0da62a29729db9d684
adraomar <adra.omar2@gmail.com>
2022-08-28 23:03:20 -0300

    se quitaron elementos que no se usaban

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 const ItemCount = (props) => {
     const [contador, setContador] = useState(props.initial);
