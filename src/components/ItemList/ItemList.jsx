68afbb83b193de56a824f505d55ca5fb2e397c32
Adra Omar <adra.omar2@gmail.com>
2022-09-26 18:37:04 -0300

    Correcioens menores

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -7,7 +7,7 @@ const ItemList = ({ items }) => {
             <div className="row row-cols-1 row-cols-md-3 g-2">
                 {
                     items.map((elemento, index) =>
-                        <Item key={index} item={elemento}></Item>
+                        <Item key={index} item={elemento} idx={index}></Item>
                     )
                 }
                 {
