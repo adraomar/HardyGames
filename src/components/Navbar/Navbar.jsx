4e816c951ebebcb920ac3923edfe49e86bf42535
Adra Omar <adra.omar2@gmail.com>
2022-09-26 20:28:50 -0300

    Ajustes en totales

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/images/logo2.png';
+import logo from '../../assets/images/logo.png';
 import './navbar.css';
 import { Link } from "react-router-dom";
 
@@ -9,9 +9,7 @@ const Navbar = () => {
             <div className="container-fluid bg-dark text-white">
                 <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
                     <Link to={`/`} className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                        <div className="logo">
-                            <img src={logo} className="logo" alt="" />
-                        </div>
+                        <img src={logo} className="logo" alt="" />
                     </Link>
 
                     <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
